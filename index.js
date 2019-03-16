598a6ddd5f1b30cab8b0afba23df83cd21024cd2
Behera <inlbehera@ad.harman.com>
2019-03-16 21:12:51 +0530

    new udemy react and node cource practice

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send({hi:'There'});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+console.log('listening to the port '+PORT);
